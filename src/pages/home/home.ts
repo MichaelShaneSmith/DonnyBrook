b455c20e6fb455575bd2be9f953fa868daf30c18
M. Smith <animaniac21@gmail.com>
2017-07-16 08:27:22 -0700

    profile page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,15 +14,33 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class HomePage {
 
+	showAuth: boolean;
 	items: FirebaseListObservable<any[]>;
 	user: Observable<firebase.User>;
-	//user_email: string;
-	//user_pwd: string;
+	user_0 = {
+		name: 'Michael Smith',
+		profileImage: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/13775405_1401473769867978_5482203317042747458_n.jpg?oh=cf18bc0d2a32dc0636cfbab4687b66af&oe=59CAE525',
+		coverImage: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/13769430_1401473179868037_646529923203410133_n.jpg?oh=84af21bf109ee04ff8cdcb5920aa47d4&oe=59C4EEA4',
+		occupation: 'Male',
+		location: 'Hillsboro, Or',
+		description: 'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do',
+		followers: 456,
+		following: 1052,
+		posts: 35
+	};
+
+	language: any;
+	genre: any;
+	religion: any;
+	religions = ['Christianity', 'Islam', 'Other'];
+	languages = ['Republican', 'Democratic', 'Other'];
+	genres =['Rock', 'Rap', 'Country'];
 
 
 	constructor(public navCtrl: NavController, db: AngularFireDatabase, public afAuth: AngularFireAuth) {
 		this.items = db.list('/Test');
 		this.user = afAuth.authState;
+		this.showAuth = true;
 		console.log(this.user);
 	}
 
@@ -43,15 +61,18 @@ export class HomePage {
 			var errorMessage = error.message;
 			// ...
 		});
+		this.showAuth = false
 	}
 
 	logout() {
 		firebase.auth().signOut().then(function() {
 			// Sign-out successful.
 			console.log(this.user);
+			
 		}).catch(function(error) {
 			// An error happened.
 		});
+		this.showAuth = true;
 	}
 
 	funTest() {
